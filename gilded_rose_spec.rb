0e2f4f012b3b47f138c00d5dc33d715730a9f1eb
Javier Soto <sotoseattle@gmail.com>
2015-01-01 08:39:26 -0800

    change to minitest_spec and first failing test

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,11 +1,11 @@
 require './gilded_rose.rb'
-require "rspec"
+require "minitest/autorun"
 
 describe GildedRose do
+  subject { GildedRose.new }
 
-  it "should do something" do
-    subject.update_quality
+  it 'all items have a SellIn value (number of days to sell it)' do
+    subject.items.first.must_respond_to sell_in
   end
-
 
 end
