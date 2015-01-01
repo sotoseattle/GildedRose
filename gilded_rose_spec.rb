d308bc7f64c757f45f94434a012ff3a395540c75
Javier Soto <sotoseattle@gmail.com>
2015-01-01 10:26:48 -0800

    test brie

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -9,26 +9,41 @@ describe GildedRose do
 
   it { subject.items.first.must_respond_to :quality }
 
-  it 'at the end of the day, quality decreases' do
-    initial_quality = vest.quality
-    subject.update_quality
-    vest.quality.must_equal initial_quality - 1
+  describe 'standard items' do
+    it 'at the end of the day, quality decreases' do
+      initial_quality = vest.quality
+      subject.update_quality
+      vest.quality.must_equal initial_quality - 1
+    end
+
+    it 'at the end of the day, the sell_in date decreases' do
+      initial_sell_in = vest.sell_in
+      subject.update_quality
+      vest.sell_in.must_equal initial_sell_in  - 1
+    end
+
+    it 'once the sell_in reaches 0, quality falls twice as fast' do
+      vest_at_expiration.quality.must_equal 10
+      subject.update_quality
+      vest.quality.must_equal 8
+    end
+
+    it 'quality cannot be negative' do
+      (vest.quality + vest.sell_in).times { subject.update_quality }
+      vest.quality.must_equal 0
+    end
   end
 
-  it 'at the end of the day, the sell_in date decreases' do
-    initial_sell_in = vest.sell_in
-    subject.update_quality
-    vest.sell_in.must_equal initial_sell_in  - 1
-  end
+  describe 'special case: Brie' do
+    it 'brie is special, its quality grows up' do
+      initial_quality = brie.quality
+      subject.update_quality
+      brie.quality.must_equal initial_quality + 1
+    end
 
-  it 'once the sell_in reaches 0, quality falls twice as fast' do
-    vest_at_expiration.quality.must_equal 10
-    subject.update_quality
-    vest.quality.must_equal 8
-  end
-
-  it 'quality cannot be negative' do
-    (vest.quality + vest.sell_in).times { subject.update_quality }
-    vest.quality.must_equal 0
+    it 'but quality cannot exceed 50' do
+      50.times { subject.update_quality }
+      brie.quality.must_equal 50
+    end
   end
 end
