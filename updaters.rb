c26670322e8cbd6c0c7580f89c66f6d5c6f45141
Javier Soto <sotoseattle@gmail.com>
2015-01-01 13:18:39 -0800

    refactor for dry

--- updaters.rb
+++ updaters.rb
@@ -1,4 +1,10 @@
 module Basic_Updater
+  def update!
+    one_day_passes
+    update_quality
+    verify_quality_limits
+  end
+
   def one_day_passes
     @sell_in -= 1
   end
@@ -11,19 +17,15 @@ end
 
 module Standard
   include Basic_Updater
-  def update!
-    one_day_passes
+  def update_quality
     @quality -= (@sell_in >= 0 ? 1 : 2)
-    verify_quality_limits
   end
 end
 
 module Cheesy
   include Basic_Updater
-  def update!
-    one_day_passes
+  def update_quality
     @quality += 1
-    verify_quality_limits
   end
 end
 
@@ -34,25 +36,20 @@ end
 
 module Tickety
   include Basic_Updater
-  def update!
-    one_day_passes
-
+  def update_quality
     return @quality = 0 if @sell_in <= 0
     case @sell_in
     when (1..5)  then @quality += 3
     when (6..10) then @quality += 2
     else              @quality += 1
     end
-    verify_quality_limits
   end
 end
 
 module Conjured
   include Basic_Updater
-  def update!
-    one_day_passes
+  def update_quality
     2.times { @quality -= (@sell_in >= 0 ? 1 : 2) }
-    verify_quality_limits
   end
 end
 
