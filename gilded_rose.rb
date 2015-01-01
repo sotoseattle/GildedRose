d38ed5af754273789a230b8750869d9db2fd60c0
Javier Soto <sotoseattle@gmail.com>
2015-01-01 11:35:56 -0800

    basic refactoring

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -17,50 +17,46 @@ class GildedRose
 
   def update_quality
     items.each do |item|
-      if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert")
-        if (item.quality > 0)
-          if (item.name != "Sulfuras, Hand of Ragnaros")
-            item.quality = item.quality - 1
-          end
-        end
+      one_day_passes(item)
+
+      case item.name
+      when 'Aged Brie' then update_brie(item)
+      when 'Sulfuras, Hand of Ragnaros' then update_sulfuras(item)
+      when 'Backstage passes to a TAFKAL80ETC concert' then update_concert(item)
+      when 'Conjured Mana Cake' then update_conjure(item)
       else
-        if (item.quality < 50)
-          item.quality = item.quality + 1
-          if (item.name == "Backstage passes to a TAFKAL80ETC concert")
-            if (item.sell_in < 11)
-              if (item.quality < 50)
-                item.quality = item.quality + 1
-              end
-            end
-            if (item.sell_in < 6)
-              if (item.quality < 50)
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if (item.name != "Sulfuras, Hand of Ragnaros")
-        item.sell_in = item.sell_in - 1;
-      end
-      if (item.sell_in < 0)
-        if (item.name != "Aged Brie")
-          if (item.name != "Backstage passes to a TAFKAL80ETC concert")
-            if (item.quality > 0)
-              if (item.name != "Sulfuras, Hand of Ragnaros")
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if (item.quality < 50)
-            item.quality = item.quality + 1
-          end
-        end
+        update_standard(item)
       end
     end
   end
 
+  def one_day_passes(item)
+    item.sell_in -= 1
+  end
+
+  def update_standard(item)
+    item.quality -= (item.sell_in >= 0 ? 1 : 2)
+    item.quality = 0 if item.quality < 0
+  end
+
+  def update_brie(item)
+    item.quality += 1 unless item.quality == 50
+  end
+
+  def update_sulfuras(item)
+    item.sell_in = 0
+  end
+
+  def update_concert(item)
+    return item.quality = 0 if item.sell_in <= 0
+
+    case item.sell_in
+    when (1..5)  then item.quality += 3
+    when (6..10) then item.quality += 2
+    else              item.quality += 1
+    end
+  end
+
+  def update_conjure(item)
+  end
 end
